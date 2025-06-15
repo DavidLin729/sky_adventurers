4745224eaa485ef2f4cfa263fa443b75c17c5ea0
DavidLin729 <yuyang0729@gmail.com>
2025-06-15 12:33:32 +0800

    更新：修改 app.py 的 host 設定

--- app.py
+++ app.py
@@ -573,4 +573,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         init_admin()  # 初始化管理員帳號
-    app.run(debug=True) 
+    app.run(host='0.0.0.0', port=5000, debug=True) 
